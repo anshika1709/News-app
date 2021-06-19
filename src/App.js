880526a6034f8ec17d14b68d826e1ef780d23b0c
anshika1709 <awasthi.anshika1709@gmail.com>
2021-06-19 17:27:03 +0530

    Add news Cards

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import alanBtn from "@alan-ai/alan-sdk-web";
+import NewsCards from "./Components/NewsCards/NewsCards";
 
-const alanKey = "";
-//   "2f53941f5f94bf33159642fe5403a4002e956eca572e1d8b807a3e2338fdd0dc/stage";
+const alanKey =
+  "2f53941f5f94bf33159642fe5403a4002e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 const App = () => {
+  const [newsArticles, setNewsArticles] = useState([]);
   useEffect(() => {
     alanBtn({
       key: alanKey,
-      onCommand: ({ command }) => {
-        if (command === "testCommand") alert("Executed!");
+      onCommand: ({ command, articles }) => {
+        if (command === "newHeadlines") setNewsArticles(articles);
       },
     });
   }, []);
-  return <div>Hi!</div>;
+
+  return (
+    <div>
+      <NewsCards articles={newsArticles} />
+    </div>
+  );
 };
 
 export default App;
