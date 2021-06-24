4c8d7afb52178fc4df58226fd46b8478fc1a1101
anshika1709 <awasthi.anshika1709@gmail.com>
2021-06-24 19:08:11 +0530

    Add identification of numbers for articles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 import NewsCards from "./Components/NewsCards/NewsCards";
 import useStyles from "./styles";
+import wordsToNumbers from "words-to-numbers";
+import { Typography } from "@material-ui/core";
 
 const alanKey = "2f53941f5f94bf33159642fe5403a4002e956eca572e1d8b807a3e2338fdd0dc/stage";
 
@@ -14,20 +16,38 @@ const App = () => {
   useEffect(() => {
     alanBtn({
       key: alanKey,
-      onCommand: ({ command, articles }) => {
+      onCommand: ({ command, articles, number }) => {
         if (command === "newHeadlines") {
           setNewsArticles(articles);
           setActiveArticle(-1);
-        } else if (command === "highlight") setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === "highlight") {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === "open") {
+          const parsedNumber = number.length > 2 ? wordsToNumbers(number, { fuzzy: true }) : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > articles.length) {
+            alanBtn().playText("Please try that again...");
+          } else if (article) {
+            window.open(article.url, "_blank");
+            alanBtn().playText("Opening...");
+          } else {
+            alanBtn().playText("Please try that again...");
+          }
+        }
       },
     });
   }, []);
 
   return (
-    <div className={classes.container} container>
-      <div className={classes.logoContainer}>
-        <img src="https://www.letsnurture.com/wp-content/uploads/2018/01/VE2.png" className={classes.alanLogo} alt="logo" />
-      </div>
+    <div className={classes.container}>
+      <center>
+        <Typography variant="h1"> Whats The News! </Typography>
+
+        <div className={classes.logoContainer}>
+          <img src="https://www.letsnurture.com/wp-content/uploads/2018/01/VE2.png" className={classes.alanLogo} alt="logo" />
+        </div>
+      </center>
       <NewsCards articles={newsArticles} activeArticle={activeArticle} />
     </div>
   );
